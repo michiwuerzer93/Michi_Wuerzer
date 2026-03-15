d9fd09052e014011d139a2bb38818469546922d1
michiwuerzer93 <michiwuerzer@gmail.com>
2026-03-15 11:32:35 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from './App.jsx'
 import './index.css'
+// 1. Hier den Import hinzufügen:
+import { inject } from '@vercel/analytics';
+
+// 2. Hier die Funktion aufrufen:
+inject();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 )
